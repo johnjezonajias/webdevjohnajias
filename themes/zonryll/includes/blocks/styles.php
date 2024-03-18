43059b9566e9e786e08edc6bebb2d6102f063173
John Jezon Barlisan Ajias <johnjezonajias@gmail.com>
2024-03-18 15:19:47 +0800

    Add: Block theme initial commit

--- themes/zonryll/includes/blocks/styles.php
+++ themes/zonryll/includes/blocks/styles.php
@@ -11,7 +11,33 @@ namespace Zonryll\Blocks\Styles;
  * Default setup routine.
  */
 function setup(): void {
+	add_action( 'after_setup_theme', __NAMESPACE__ . '\\enqueue_block_styles' );
 	add_action( 'init', __NAMESPACE__ . '\\add_block_styles' );
+}
+
+/**
+ * Add theme support for block styles and editor style.
+ */
+function enqueue_block_styles(): void {
+	/*
+	 * Load additional block styles.
+	 */
+	$styled_blocks = [
+		'button',
+		'quote',
+		'navigation',
+	];
+
+	foreach ( $styled_blocks as $block_name ) {
+		wp_enqueue_block_style(
+			"core/$block_name",
+			[
+				'handle' => "webicient-$block_name",
+				'src'    => ZONRYLL_DIST_URL . "/css/blocks/$block_name.css",
+				'path'   => ZONRYLL_DIST_URL . "/css/blocks/$block_name.css",
+			]
+		);
+	}
 }
 
 /**
---
+++ themes/zonryll/src/blocks/toggle-dark/render.php
@@ -0,0 +1,4 @@
+<label class="toggle-dark-btn">
+	<input type="checkbox" class="toggle-dark-mode" />
+	<span class="circle"></span>
+</label>
