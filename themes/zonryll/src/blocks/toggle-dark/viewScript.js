7de0c8f61dba0aa04f59e31eb2fb2133ba41395b
John Jezon Barlisan Ajias <johnjezonajias@gmail.com>
2024-03-19 13:54:10 +0800

    Fix: Banner responsive

--- themes/zonryll/src/blocks/toggle-dark/viewScript.js
+++ themes/zonryll/src/blocks/toggle-dark/viewScript.js
@@ -2,10 +2,42 @@ import domReady from '@wordpress/dom-ready';
 
 domReady( function () {
 	const themeToggleButtons = document.querySelectorAll( '.toggle-dark-mode' );
+	const darkModeMediaQuery = window.matchMedia(
+		'(prefers-color-scheme: dark)'
+	);
+	const body = document.body;
+
+	function applyDarkMode() {
+		body.classList.add( 'dark-mode' );
+		themeToggleButtons.forEach( ( button ) => ( button.checked = true ) );
+	}
+
+	function removeDarkMode() {
+		body.classList.remove( 'dark-mode' );
+		themeToggleButtons.forEach( ( button ) => ( button.checked = false ) );
+	}
+
+	if ( darkModeMediaQuery.matches ) {
+		applyDarkMode();
+	} else {
+		removeDarkMode();
+	}
+
+	darkModeMediaQuery.addListener( function ( event ) {
+		if ( event.matches ) {
+			applyDarkMode();
+		} else {
+			removeDarkMode();
+		}
+	} );
 
 	themeToggleButtons.forEach( function ( button ) {
 		button.addEventListener( 'click', function () {
-			document.body.classList.toggle( 'dark-mode' );
+			if ( button.checked ) {
+				applyDarkMode();
+			} else {
+				removeDarkMode();
+			}
 		} );
 	} );
 } );
