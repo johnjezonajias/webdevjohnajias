afc0d3fa403dd4da96b340413362ef515352fe3a
John Jezon Barlisan Ajias <johnjezonajias@gmail.com>
2024-03-20 21:31:51 +0800

    Format: PHP linting

--- themes/zonryll/src/blocks/back-top/render.php
+++ themes/zonryll/src/blocks/back-top/render.php
@@ -1,3 +1,12 @@
+<?php
+/**
+ * Server-side rendering of the `zonryll/back-top` block.
+ *
+ * @package Zonryll\Blocks
+ */
+
+?>
+
 <div class="back-to-top">
-    <button class="back-to-top__button"></button>
+	<button class="back-to-top__button"></button>
 </div>
--- themes/zonryll/src/blocks/scroll-progress/render.php
+++ themes/zonryll/src/blocks/scroll-progress/render.php
@@ -1,3 +1,12 @@
+<?php
+/**
+ * Server-side rendering of the `zonryll/scroll-progress` block.
+ *
+ * @package Zonryll\Blocks
+ */
+
+?>
+
 <div class="scroll-progress">
-  <div class="scroll-progress__indicator"></div>
+	<div class="scroll-progress__indicator"></div>
 </div>
