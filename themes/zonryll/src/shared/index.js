1c26ff10ed0e83938e25c40695d5722a9e447058
John Jezon Barlisan Ajias <johnjezonajias@gmail.com>
2024-03-20 16:53:38 +0800

    Feature: Implement Lenis smooth scrolling

--- themes/zonryll/src/shared/index.js
+++ themes/zonryll/src/shared/index.js
@@ -1,3 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import Lenis from '@studio-freight/lenis';
+
 import './scss/styles.scss';
 
 window.addEventListener( 'scroll', function () {
@@ -8,3 +11,12 @@ window.addEventListener( 'scroll', function () {
 		header.classList.remove( 'scrolled' );
 	}
 } );
+
+const lenis = new Lenis();
+
+function raf( time ) {
+	lenis.raf( time );
+	requestAnimationFrame( raf );
+}
+
+requestAnimationFrame( raf );
