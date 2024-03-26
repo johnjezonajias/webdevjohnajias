deab3512388dbb0b7fa8cb1b9c99d19a25b04506
John Jezon Barlisan Ajias <johnjezonajias@gmail.com>
2024-03-26 08:35:31 +0800

    Update: Banner text GSAP implementation

--- themes/zonryll/src/shared/index.js
+++ themes/zonryll/src/shared/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsdoc/require-param-type */
 /* eslint-disable import/no-extraneous-dependencies */
 import domReady from '@wordpress/dom-ready';
 import Lenis from '@studio-freight/lenis';
@@ -24,28 +25,86 @@ domReady( () => {
 	 */
 	gsap.registerPlugin( ScrollTrigger );
 
-	const text = document.querySelector( '.site-banner__contents' );
-	const tl = gsap.timeline( {
+	const bannerText = document.querySelector( '.site-banner__contents' );
+	const bannerTextTimeLine = gsap.timeline( {
 		defaults: { ease: 'none', transformOrigin: '50% 50%' },
 		scrollTrigger: {
 			trigger: '.site-banner',
 			start: 'top top',
 			end: 'bottom center',
 			scrub: true,
-			markers: true,
 		},
 	} );
 
-	tl.to(
-		text,
+	bannerTextTimeLine.to(
+		bannerText,
 		{
-			y: -text.offsetHeight * text.dataset.depth,
+			y: -bannerText.offsetHeight * bannerText.dataset.depth,
 			autoAlpha: 0,
 			scale: 1.08,
 			duration: 0.2,
 		},
 		0
 	);
+} );
+
+/**
+ * GSAP animations.
+ *
+ * @param elem
+ * @param direction
+ */
+function animateFrom( elem, direction ) {
+	direction = direction || 1;
+	let x = 0,
+		y = direction * 150;
+	if ( elem.classList.contains( 'animated--from-left' ) ) {
+		x = -150;
+		y = 0;
+	} else if ( elem.classList.contains( 'animated--from-right' ) ) {
+		x = 150;
+		y = 0;
+	}
+	elem.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+	elem.style.opacity = '0';
+	gsap.fromTo(
+		elem,
+		{ x, y, autoAlpha: 0 },
+		{
+			duration: 1.25,
+			x: 0,
+			y: 0,
+			autoAlpha: 1,
+			ease: 'expo',
+			overwrite: 'auto',
+		}
+	);
+}
+
+function hide( elem ) {
+	gsap.set( elem, { autoAlpha: 0 } );
+}
+
+document.addEventListener( 'DOMContentLoaded', function () {
+	gsap.registerPlugin( ScrollTrigger );
+
+	gsap.utils.toArray( '.animated' ).forEach( function ( elem ) {
+		hide( elem );
+
+		ScrollTrigger.create( {
+			trigger: elem,
+			markers: false,
+			onEnter() {
+				animateFrom( elem );
+			},
+			onEnterBack() {
+				animateFrom( elem, -1 );
+			},
+			onLeave() {
+				hide( elem );
+			},
+		} );
+	} );
 } );
 
 /**
