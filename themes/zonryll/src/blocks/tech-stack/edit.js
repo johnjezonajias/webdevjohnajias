f63912c48c8ffdf37a4a28147d03fe059e14a5a2
John Jezon Barlisan Ajias <johnjezonajias@gmail.com>
2024-03-26 08:32:07 +0800

    Add: Tech-stacks block implementation

---
+++ themes/zonryll/src/blocks/tech-stack/edit.js
@@ -0,0 +1,41 @@
+/**
+ * WordPress dependencies.
+ */
+import { useBlockProps, RichText, InnerBlocks } from '@wordpress/block-editor';
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './editor.scss';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @param {Object} param
+ * @param {Object} param.attributes
+ * @param {Object} param.setAttributes
+ * @return {WPElement} Element to render.
+ */
+export default function Edit( { attributes, setAttributes } ) {
+	return (
+		<div { ...useBlockProps( { className: 'tech-stack' } ) }>
+			<div className="tech-stack__card">
+				<InnerBlocks />
+				<h5>
+					<RichText
+						tagName="span"
+						allowedFormats={ [] }
+						value={ attributes.title }
+						onChange={ ( title ) => setAttributes( { title } ) }
+						formattingControls={ [] }
+						placeholder="Title"
+					/>
+				</h5>
+			</div>
+		</div>
+	);
+}
---
+++ themes/zonryll/src/blocks/tech-stack/render.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Server-side rendering of the `zonryll/skill` block.
+ *
+ * @package Zonryll\Blocks
+ */
+
+namespace Zonryll\Block\Skill;
+
+if ( ! $attributes['title'] ) {
+	return;
+}
+
+?>
+<div <?php echo get_block_wrapper_attributes( [ 'class' => 'tech-stack' ] ); ?>>
+	<div class="tech-stack__card">
+		<?php
+			foreach ( $block->inner_blocks as $inner_block ) {
+				echo $inner_block->render();
+			}
+		?>
+		<h5><?php echo $attributes['title']; ?></h5>
+	</div>
+</div>
